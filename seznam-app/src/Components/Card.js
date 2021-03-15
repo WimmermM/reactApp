b7f3493a40f2fe044139e09e2bad68c924a79b3b
WimmermM <marek.wimmer@gmail.com>
2021-03-15 10:25:51 +0100

    new component

---
+++ seznam-app/src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Card(title, imageUrl, body) {
+    return (
+        <div className='card-container'>
+            <div className='image-container'>
+                <img src={imageUrl} alt='' />
+            </div>
+
+        </div>
+    )
+}
+
+export default Card;
